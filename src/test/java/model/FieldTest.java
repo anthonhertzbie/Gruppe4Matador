c550fd0801633d5fec0da17b75e7524b089c7635
laerkekragskov <113907437+laerkekragskov@users.noreply.github.com>
2023-01-10 14:26:05 +0100

    New test branch branch(field class)

---
+++ src/test/java/model/FieldTest.java
@@ -0,0 +1,13 @@
+package java.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldTest {
+
+    @Test
+    @DisplayName("")
+
+}
