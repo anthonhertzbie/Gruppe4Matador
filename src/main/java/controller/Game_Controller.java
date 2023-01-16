0f15066087cadc0e92eaaa628aa6b94df18c4562
CrazyCows <emil.s.simonsen@gmail.com>
2023-01-17 00:51:02 +0100

    Fixed buying and selling of hotels. Rent is now accurate and game wont crash if selling/buying too many houses.

--- src/main/java/controller/Game_Controller.java
+++ src/main/java/controller/Game_Controller.java
@@ -83,6 +83,7 @@ public class Game_Controller {
             choice = userIO.getUserButtonPressed(currentName + "'s tur.", "Rull med tærningerne", "Byg huse");
             switch(choice) {
                 case "Rull med tærningerne":
+                    userIO.showMessage("Rul tærningerne!");
                     return;
                 case "Byg huse":
                     while (true) {
@@ -101,6 +102,7 @@ public class Game_Controller {
             choice = userIO.getUserButtonPressed(currentName + "'s tur.", "Rull med tærningerne", "Byg huse", "Sælg huse");
             switch(choice) {
                 case "Rull med tærningerne":
+                    userIO.showMessage("Rul tærningerne!");
                     return;
                 case "Byg huse":
                     while (true) {
@@ -169,14 +171,6 @@ public class Game_Controller {
         model.changeTurn();
     }
 
-    public void fielLogic(){
-
-    }
-
-    public void setPlayerFreeFromJail(){
-        model.getPlayerCurrentTurn().setInJail(false);
-    }
-
     /**
      * This only needs to be called !!FIRST THING!! after the current player has moved.
      */
@@ -222,17 +216,11 @@ public class Game_Controller {
                 notifyEverything();
             }
         }
-
     }
-
-
-
     // Sets the total amounts of playeres allowed in the current game
     public void setTotalPlayerCount(String totalPlayerCount){
         model.setTotalPlayerCount(Integer.parseInt(totalPlayerCount));
     }
-
-
     // Rolls the dices and updates the view
     public void diceRoll(){
         model.getCup().rollDices();
@@ -244,88 +232,4 @@ public class Game_Controller {
         model.getPlayerByIndex(index).setPlayerID(index);
         notifyEverything();
     }
-
-
-
-
-
-/*
-    public void checkIfGroupsIsOwned(){
-        Gameboard board = model.gameBoard();
-
-        for (int i = 0; i < board.getOwnerOfFieldGroups().length; i ++) {
-            switch (field) {
-                case 1, 3 -> {
-                    if (board.whoOwnsThis(1) == board.whoOwnsThis(3) && board.whoOwnsThis(1) == i) {
-                        board.setOwnerOfFieldgroups(i, 0);
-                    } else if (board.whoOwnsThis(1) != board.whoOwnsThis(3)){
-                        board.setOwnerOfFieldgroups(-1, 0);
-                    }
-                    return;
-                }
-                case 6, 8, 9 -> {
-                    if (board.whoOwnsThis(6) == board.whoOwnsThis(8) && board.whoOwnsThis(8) == board.whoOwnsThis(9) && board.whoOwnsThis(6) == i) {
-                        board.setOwnerOfFieldgroups(i, 1);
-                    } else if (board.whoOwnsThis(6) != board.whoOwnsThis(8) || board.whoOwnsThis(8) != board.whoOwnsThis(9)){
-                        board.setOwnerOfFieldgroups(-1, 1);
-                    }
-                    return;
-                }
-                case 11, 13, 14 -> {
-                    if (board.whoOwnsThis(11) == board.whoOwnsThis(13) && board.whoOwnsThis(13) == board.whoOwnsThis(14) && board.whoOwnsThis(11) == i) {
-                        board.setOwnerOfFieldgroups(i, 2);
-                    } else if (board.whoOwnsThis(11) != board.whoOwnsThis(13) || board.whoOwnsThis(13) != board.whoOwnsThis(14)){
-                        board.setOwnerOfFieldgroups(-1, 2);
-                    }
-                    return;
-                }
-
-                case 16, 18, 19 -> {
-                    if (board.whoOwnsThis(16) == board.whoOwnsThis(18) && board.whoOwnsThis(18) == board.whoOwnsThis(19) && board.whoOwnsThis(16) == i) {
-                        board.setOwnerOfFieldgroups(i, 3);
-                    } else if (board.whoOwnsThis(16) != board.whoOwnsThis(18) || board.whoOwnsThis(18) != board.whoOwnsThis(19)){
-                        board.setOwnerOfFieldgroups(-1, 3);
-                    }
-                    return;
-                }
-                case 21, 23, 24 -> {
-                    if (board.whoOwnsThis(21) == board.whoOwnsThis(23) && board.whoOwnsThis(23) == board.whoOwnsThis(24) && board.whoOwnsThis(21) == i) {
-                        board.setOwnerOfFieldgroups(i, 4);
-                    } else if (board.whoOwnsThis(21) != board.whoOwnsThis(23) || board.whoOwnsThis(23) != board.whoOwnsThis(24)){
-                        board.setOwnerOfFieldgroups(-1, 4);
-                    }
-                    return;
-                }
-                case 26, 27, 29 -> {
-                    if (board.whoOwnsThis(26) == board.whoOwnsThis(27) && board.whoOwnsThis(27) == board.whoOwnsThis(29) && board.whoOwnsThis(26) == i) {
-                        board.setOwnerOfFieldgroups(i, 5);
-                    } else if (board.whoOwnsThis(26) != board.whoOwnsThis(27) || board.whoOwnsThis(27) != board.whoOwnsThis(29)){
-                        board.setOwnerOfFieldgroups(-1, 5);
-                    }
-                    return;
-                }
-                case 31, 32, 34 -> {
-                    if (board.whoOwnsThis(31) == board.whoOwnsThis(32) && board.whoOwnsThis(32) == board.whoOwnsThis(34) && board.whoOwnsThis(31) == i) {
-                        board.setOwnerOfFieldgroups(i, 6);
-                    } else if (board.whoOwnsThis(31) != board.whoOwnsThis(32) || board.whoOwnsThis(32) != board.whoOwnsThis(34)){
-                        board.setOwnerOfFieldgroups(-1, 6);
-                    }
-                    return;
-                }
-                case 37, 39 -> {
-                    if (board.whoOwnsThis(37) == board.whoOwnsThis(39) && board.whoOwnsThis(37) == i) {
-                        board.setOwnerOfFieldgroups(i, 7);
-                    } else if (board.whoOwnsThis(37) != board.whoOwnsThis(38)){
-                        board.setOwnerOfFieldgroups(-1, 7);
-                    }
-                    return;
-                }
-            }
-        }
-    }
-
- */
-
-
-
 }
